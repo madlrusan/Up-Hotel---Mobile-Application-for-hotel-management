854e8dd22df222705910a50d8e82a43493c3ca12
Adelin Chis <adelingabriel08@gmail.com>
2022-10-23 20:52:20 +0300

    Adding Identity & Database configuration

--- Backend/UpHotel.API/Program.cs
+++ Backend/UpHotel.API/Program.cs
@@ -1,3 +1,5 @@
+using UpHotel.Business.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddBusinessServices(builder.Configuration.GetConnectionString("UpHotel"));
 
 var app = builder.Build();
 
---
+++ Backend/UpHotel.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UpHotel.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime CreatedAtTimeUtc { get; set; }
+    }
+}
