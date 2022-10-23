9aa6d269a3a94c8821843147acb11bbaf634209e
Ioana Rusan <ioana.rusan@student.upt.ro>
2022-10-23 20:53:47 +0300

    init first view of login screen

--- Frontend/UpHotelFE/App.tsx
+++ Frontend/UpHotelFE/App.tsx
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './AppStyles';
 export default function App() {
+    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
       <Text>UpHotel</Text>
       <StatusBar style="auto" />
+      <View style={styles.inputView}>
+
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Frontend/UpHotelFE/AppStyles.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native"
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#5856BB',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputView: {
+    backgroundColor: "#ffffff",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "center",
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    alignItems:"center",
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#ffffff",
+  },
+
+  loginText: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    alignItems:"center",
+    justifyContent: "center",
+  },
+});
