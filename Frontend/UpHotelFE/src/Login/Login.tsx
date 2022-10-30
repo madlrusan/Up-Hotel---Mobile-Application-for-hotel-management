0f67eb088f8690fe659065d391745577857ebd21
Ioana Rusan <ioana.rusan@student.upt.ro>
2022-10-30 17:04:03 +0200

    implemented login screen without functionality

---
+++ Frontend/UpHotelFE/src/Login/Login.tsx
@@ -0,0 +1,99 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { formStyles, styles } from "./LoginStyles";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { TextInput } from "react-native-paper";
+import "../../assets/Logo.png";
+export const Login = ()=>{
+	const [state, setState] = useState<LoginState>({
+		loginCredentials: { email: "", password: "" },
+		isSubmitted: true,
+	});
+	const onSubmit = () => {
+		setState((prevState) => {
+			return { ...prevState, isSubmitted: true };
+		});
+		// context.login(
+		// 	state.loginCredentials.email,
+		// 	state.loginCredentials.password
+		// );
+		setState((prevState) => {
+			return { ...prevState, isSubmitted: false };
+		});
+	};
+	return (
+		<>
+			<LinearGradient
+			// Background Linear Gradient
+				colors={["#5856BB", "#E2DA92"]}
+				start={{x:0, y:0}}
+				end={{x:0, y:1}}
+				style={styles.container}
+			>
+				<Text style={styles.logoText}> UpHotel </Text>
+				<Image style={styles.logoImg} source={require("../../assets/Logo.png")}/>
+				<View style={formStyles.loginBox}>
+					<Text style={formStyles.formHeader}>Welcome to UpHotel!</Text>
+					<Text style={formStyles.formSubHeader}>Please Login with given credentials</Text>
+					<TextInput
+						label="Email address"
+						value={state.loginCredentials.email}
+						mode="outlined"
+						onChangeText={(text: string) => 
+							setState((prevState) => {
+								return {
+									...prevState,
+									loginCredentials: {
+										email: text,
+										password: prevState.loginCredentials.password,
+									},
+								};
+							})}
+						style={formStyles.formBox}
+						keyboardType="email-address"
+
+					/>
+			
+					<TextInput 
+						label="Password"
+						mode="outlined"
+						value={state.loginCredentials.password}
+						onChangeText={(text) =>
+							setState((prevState) => {
+								return {
+									...prevState,
+									loginCredentials: {
+										password: text,
+										email: prevState.loginCredentials.email,
+									},
+								};
+							})
+						}
+						style={formStyles.formBox}
+						secureTextEntry={true}/>
+
+					<TouchableOpacity
+						onPress={onSubmit}
+						disabled={state.isSubmitted}
+						style={
+							!state.isSubmitted
+								? formStyles.formSubmitButton
+								: formStyles.formSubmitButtonDisabled
+						}
+					>
+						<Text style={formStyles.formSubmitButtonText}>Sign in</Text>
+					</TouchableOpacity>
+				</View>
+			</LinearGradient>
+		</>
+	);
+};
+
+type loginCredentialsType = {
+  email: string;
+  password: string;
+};
+type LoginState = {
+  loginCredentials: loginCredentialsType;
+  isSubmitted: boolean;
+};
---
+++ Frontend/UpHotelFE/src/Login/LoginStyles.ts
@@ -0,0 +1,93 @@
+import { Dimensions, Platform, StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	logoText: {
+		color: "rgba(222, 224, 150, 1)",
+		alignItems : "center",
+		textAlign: "center",
+		fontSize: 96,
+		letterSpacing: 0.46,
+	},
+	logoImg: {
+		paddingVertical: 20,
+	}
+});
+
+export const formStyles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	loginBox: {
+		width: (Dimensions.get("window").width * 8) / 10,
+		height:
+      Platform.OS === "ios" ? (Dimensions.get("window").height * 5) / 12 + 25 : (Dimensions.get("window").height * 5) / 12,
+		backgroundColor: "#fff",
+		borderRadius: 20,
+		padding: 16,
+		marginTop: 70,
+	},
+	registerBox: {
+		width: (Dimensions.get("window").width * 8) / 10,
+		height:
+      Platform.OS === "ios" ? (Dimensions.get("window").height * 2) / 3 + 25 : (Dimensions.get("window").height * 2) / 3,
+		backgroundColor: "rgba(255, 255, 255, 0.6)",
+		borderRadius: 20,
+		padding: 16,
+	},
+	formHeader: {
+		color: "rgb(67, 89, 110)",
+		textAlign: "center",
+		marginBottom: 16,
+		fontSize: 24,
+		fontWeight: "400",
+	},
+	formSubHeader: {
+		color: "rgb(67, 89, 110)",
+		textAlign: "center",
+		marginBottom: 16,
+		fontSize: 18,
+		fontWeight: "200",
+	},
+	formBox: {
+		// padding: 5,
+		// textAlign: "left",
+		marginBottom: 0,
+		marginTop: 5,
+	},
+	formRedirectionText: {
+		color: "rgb(46,49,146)",
+		textAlign: "left",
+		fontSize: 16,
+		fontWeight: "bold",
+		paddingBottom: 10,
+	},
+	formSubmitButton: {
+		elevation: 8,
+		backgroundColor: "rgb(131,127,175)",
+		borderRadius: 5,
+		paddingVertical: 10,
+		paddingHorizontal: 12,
+		marginVertical: 90,
+	},
+	formSubmitButtonDisabled: {
+		elevation: 8,
+		backgroundColor: "rgb(188,187,197)",
+		borderRadius: 5,
+		paddingVertical: 10,
+		paddingHorizontal: 12,
+		marginVertical: 90,
+	},
+	formSubmitButtonText: {
+		fontSize: 18,
+		color: "#fff",
+		alignSelf: "center",
+		textTransform: "uppercase",
+	},
+});
